c715e30968b9b49e547f812bb986af5ee5e45d00
tonycl.wu@mail.utoronto.ca <tonycl.wu@mail.utoronto.ca>
2020-05-13 14:46:06 -0400

    First commit

---
+++ accounting-backend/src/main/java/com/personal/accountingbackend/AccountingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.personal.accountingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingBackendApplication.class, args);
+	}
+
+}
