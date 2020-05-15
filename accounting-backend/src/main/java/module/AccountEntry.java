b535c29bddd67435462da30ccd2dd7fc38c2a925
tonycl.wu@mail.utoronto.ca <tonycl.wu@mail.utoronto.ca>
2020-05-14 21:43:35 -0400

    Add hardcoded service

---
+++ accounting-backend/src/main/java/module/AccountEntry.java
@@ -0,0 +1,17 @@
+package module;
+
+import java.time.LocalDate;
+
+public class AccountEntry {
+    private Long id;
+    private LocalDate date;
+    private String entryName;
+    private double amount;
+
+    public AccountEntry(Long id, LocalDate date, String entryName, double amount) {
+        this.id = id;
+        this.date = date;
+        this.entryName = entryName;
+        this.amount = amount;
+    }
+}
